18db1ccae843bd4391254034dcdc7e03d876630f
Wundawulff <cryptojovo@gmail.com>
2026-08-24 22:51:02 +0200

    Add files via upload

---
+++ plugin/python/contract/proto/__init__.py
@@ -0,0 +1,105 @@
+"""
+Protobuf bindings for Canopy Plugin Protocol
+
+This module contains the generated protobuf classes for communication
+between the plugin and FSM.
+"""
+
+# Import generated protobuf classes
+from .account_pb2 import Account, Pool  # type: ignore[attr-defined]
+from .event_pb2 import Event, EventCustom  # type: ignore[attr-defined]
+from .tx_pb2 import (  # type: ignore[attr-defined]
+    Transaction,
+    MessageSend,
+    FeeParams,
+    Signature,
+    # ANIMI Chain custom types
+    MessageSubmit,
+    MessageVote,
+    Submission,
+    Vote,
+)
+
+# Import plugin proto classes
+from .plugin_pb2 import (  # type: ignore[attr-defined]
+    # Plugin communication types
+    FSMToPlugin,
+    PluginToFSM,
+    PluginConfig,
+    PluginFSMConfig,
+    # Request/Response types
+    PluginGenesisRequest,
+    PluginGenesisResponse,
+    PluginBeginRequest,
+    PluginBeginResponse,
+    PluginCheckRequest,
+    PluginCheckResponse,
+    PluginDeliverRequest,
+    PluginDeliverResponse,
+    PluginEndRequest,
+    PluginEndResponse,
+    PluginError,
+    # Detached query types
+    PluginQueryRequest,
+    PluginQueryResponse,
+    # State management types
+    PluginStateReadRequest,
+    PluginStateReadResponse,
+    PluginStateWriteRequest,
+    PluginStateWriteResponse,
+    PluginKeyRead,
+    PluginRangeRead,
+    PluginReadResult,
+    PluginSetOp,
+    PluginDeleteOp,
+    PluginStateEntry,
+)
+
+__all__ = [
+    # Account types
+    "Account",
+    "Pool",
+    # Event types
+    "Event",
+    "EventCustom",
+    # Transaction types
+    "Transaction",
+    "MessageSend",
+    "MessageSubmit",
+    "MessageVote",
+    "Submission",
+    "Vote",
+    "FeeParams",
+    "Signature",
+    # Plugin communication types
+    "FSMToPlugin",
+    "PluginToFSM",
+    "PluginConfig",
+    "PluginFSMConfig",
+    # Request/Response types
+    "PluginGenesisRequest",
+    "PluginGenesisResponse",
+    "PluginBeginRequest",
+    "PluginBeginResponse",
+    "PluginCheckRequest",
+    "PluginCheckResponse",
+    "PluginDeliverRequest",
+    "PluginDeliverResponse",
+    "PluginEndRequest",
+    "PluginEndResponse",
+    "PluginError",
+    # Detached query types
+    "PluginQueryRequest",
+    "PluginQueryResponse",
+    # State management types
+    "PluginStateReadRequest",
+    "PluginStateReadResponse",
+    "PluginStateWriteRequest",
+    "PluginStateWriteResponse",
+    "PluginKeyRead",
+    "PluginRangeRead",
+    "PluginReadResult",
+    "PluginSetOp",
+    "PluginDeleteOp",
+    "PluginStateEntry",
+]
